ecbcab7b4ffa1849017531d0476db22e2d35bca8
EstelleR <roges.estelle@gmail.com>
2022-08-31 13:54:26 +0200

    Code link changed

--- src/components/attribution/Attribution.jsx
+++ src/components/attribution/Attribution.jsx
@@ -9,7 +9,7 @@ const Attribution = () => {
         <NavLink to="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </NavLink>
-        . Coded by <NavLink to="#">Teru-san</NavLink>.
+        . Coded by <NavLink to="https://www.frontendmentor.io/solutions/room-home-page-react-sass-LNKrWXgKkK">EstelleR</NavLink>.
       </div>
     </>
   );
